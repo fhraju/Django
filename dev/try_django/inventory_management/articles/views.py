f16ad42c977aa41498e6c007a8709e0ab24d44d5
fhraju <alonghuman@gmail.com>
2022-08-30 12:46:59 +0600

    template

--- dev/try_django/inventory_management/articles/views.py
+++ dev/try_django/inventory_management/articles/views.py
@@ -1,8 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 from .models import Article
 
 # Create your views here.
+def article_search_view(request):
+    context = {}
+    return render(request, "articles/search.html", context=context)
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
