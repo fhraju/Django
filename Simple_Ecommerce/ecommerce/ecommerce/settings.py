dd2cba85a386d9bcc0a49f393392bcfcc1d88f69
fhraju <alonghuman@gmail.com>
2022-09-30 23:59:42 +0600

    user_cart

--- Simple_Ecommerce/ecommerce/ecommerce/settings.py
+++ Simple_Ecommerce/ecommerce/ecommerce/settings.py
@@ -120,6 +120,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 
+MEDIA_ROOT = 'static/images'
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
